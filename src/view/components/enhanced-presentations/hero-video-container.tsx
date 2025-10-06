023e6b23634454e821612187d1906ee37506fee4
bartu <bartukutlu@rightsoft.co.uk>
2025-10-06 12:54:31 +0300

    optimize first image

--- src/view/components/enhanced-presentations/hero-video-container.tsx
+++ src/view/components/enhanced-presentations/hero-video-container.tsx
@@ -1,6 +1,4 @@
 import heroVideo from '@/assets/v3/hero-noframe.mp4';
-import heroFrame from '@/assets/v3/hero-frame.png';
-import heroFirstFrame from '/hero_first_frame.webp';
 import { useState } from 'react';
 export default function HeroVideoContainer() {
     const [showVideo, setShowVideo] = useState(false);
@@ -9,7 +7,7 @@ export default function HeroVideoContainer() {
     }
     return (
         <div className="relative">
-            <img src={heroFirstFrame} fetchPriority='high' className="absolute w-[62rem] top-[6.3rem] left-[4.3rem] z-[-1]  h-auto  lg:top-[13%] lg:left-[6%] lg:w-[88%]" />
+            <img src="/hero_first_frame.webp" fetchPriority='high' className="absolute w-[62rem] top-[6.3rem] left-[4.3rem] z-[-1]  h-auto  lg:top-[13%] lg:left-[6%] lg:w-[88%]" />
             <video
                 src={heroVideo}
                 className={`absolute w-[62rem] top-[6.3rem] left-[4.3rem] z-[-1]  h-auto  lg:top-[13%] lg:left-[6%] lg:w-[88%] ${showVideo ? 'block' : 'hidden'}`}
@@ -22,7 +20,7 @@ export default function HeroVideoContainer() {
                 preload="auto"
                 onLoadedData={onVideoLoaded}
             />
-            <img src={heroFrame} fetchPriority='high' className="w-[70.21rem] max-w-[1200px] h-auto lg:w-[120vw] object-contain" alt="" />
+            <img src={"/hero-frame.png"} fetchPriority='high' className="w-[70.21rem] max-w-[1200px] h-auto lg:w-[120vw] object-contain" alt="" />
         </div>
     )
 }
