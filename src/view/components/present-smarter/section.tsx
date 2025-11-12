da2cd38e7f9429d56429c7d73c28e84cbad6bd70
usmanface44 <usman@face44.be>
2025-11-12 19:29:40 +0500

    updating padding top for scrolling card.

--- src/view/components/present-smarter/section.tsx
+++ src/view/components/present-smarter/section.tsx
@@ -76,8 +76,9 @@ export default function PresentSmarterSection() {
           className="
             lg:bg-none! lg:h-auto! relative z-[8]
             flex justify-start
-            pt-[7.5rem]! pl-[46vw] 2xl:pl-[48vw] lg:p-5 lg:pt-0!
+            pt-[11.3rem]! pl-[46vw] 2xl:pl-[48vw] lg:p-5 lg:pt-0!
             4k:[padding-left:clamp(46vw,calc(46vw+((100vw-2050px)/2000px)*3vw),50vw)]
+            4k:[padding-top:clamp(11.3rem,calc(11.3rem+((100vw-2050px)/2000px)*11.3rem),22.6vw)]
             lg:before:content-[''] 
             lg:before:absolute lg:before:left-0 lg:before:top-0
             lg:before:w-full lg:before:h-[45rem]
@@ -92,7 +93,7 @@ export default function PresentSmarterSection() {
               4k:[height:clamp(59rem,calc(59rem+((100vw-2050px)/2000px)*59rem),118rem)]
             "
           >
-            <div className="image-wrap absolute left-0 top-0 w-[50vw] object-top object-cover overflow-hidden lg:hidden">
+            <div className="image-wrap absolute left-0 top-0 w-[56vw] object-top object-cover overflow-hidden lg:hidden">
               <div
                 className="
                   w-full h-[35rem] overflow-hidden
